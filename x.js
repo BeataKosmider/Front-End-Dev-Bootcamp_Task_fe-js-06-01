b142e6bf96dfe02a3ed8dbeec20c810021df964a
Beata Kosmider <kosmibe@gmail.com>
2023-02-28 16:37:28 +0100

    task is done

--- x.js
+++ x.js
@@ -1,1 +1,15 @@
-import Html from "./index.html";
+const btn = document.querySelector("#searchKitty");
+const imageContainer = document.querySelector("#image");
+const apiUrl = "https://api.thecatapi.com/v1/images/search";
+const fetchCatImage = () => {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      const CatImage = json[0];
+      const img = document.createElement("img");
+      img.setAttribute("src", CatImage.url);
+      img.setAttribute("data-test", "img-kitty");
+      imageContainer.appendChild(img);
+    });
+};
+btn.addEventListener("click", fetchCatImage);
